af512b5bb098b4e7a87894e81b1a1fbb8d92e3ac
Laura Patron <69320369+patron02@users.noreply.github.com>
2023-04-08 13:15:58 -0500

    Update rover.py

--- rover.py
+++ rover.py
@@ -1,73 +1,52 @@
+import tkinter as tk
 import RPi.GPIO as gpio
-import time
 
-gpio.setmode(gpio.BCM)
-
-# Set default PWM settings
-frequency = 1500    # Hz
-duty_cycle = 0.75   # 75%
+# Motor Connections on Pi
+# motor M1: pins 20 and 21  (Back Right)
+# motor M2: pins 13 and 26  (Back Left)
+# motor M3: pins 25 and 16   (Front Right)
+# motor M4: pins 9 and 11   (Front Left)
 
 # Define pin mapping using a dictionary
 pin_mapping = {
-    'w': [7, 11, 15, 21],  # Forward \
-    's': [8, 12, 16, 22, 33],  # Back \
-    'a': [8, 11, 15, 22],  # Left \
-    'd': [7, 12, 16, 21],  # Right \
-
-    'e': [7, 21],  # Forward Right \
-    'z': [8, 22, 33],  # Back Left \
-    'q': [11, 15],  # Forward Left \
-    'c': [12, 16, 33],  # Back Right \
+    'w': [9, 13, 20, 25],  # Forward \
+    's': [11, 16, 21, 26],  # Back \
+    'a': [11, 13, 21, 25],  # Left \
+    'd': [9, 16, 20, 26],  # Right \
     
-    ']': [8, 11, 16, 21],  # Rotate Right \
-    '[': [7, 12, 15, 22],  # Rotate Left \
+    'e': [9, 20],  # Forward Right \
+    'z': [11, 21],  # Back Left \
+    'q': [13, 25],  # Forward Left \
+    'c': [16, 26],  # Back Right \
+    
+    ']': [9, 13, 16, 21],  # Rotate Right \
+    '[': [11, 20, 25, 26],  # Rotate Left \
 }
 
 # Setup pins as outputs
+gpio.setmode(gpio.BCM)
 for pins in pin_mapping.values():
     for pin in pins:
         gpio.setup(pin, gpio.OUT) #defines all listed pins as Outputs
+        gpio.output(pin, gpio.LOW) #set all pins LOW by default
 
-# Define key change duty cycle callback function
-def key_change_dc(key):
-    global duty_cycle
-    if key.isdigit():
-        num = int(key)
-        if num == 0:
-            duty_cycle = 1.0
-        else:
-            duty_cycle = num / 10.0
-            
-# Define key press callback function
-def key_press(key):
-    global duty_cycle
-    pins = pin_mapping.get(key) #assign pins per key press
-    if pins:
-        for pin in pins:
-            pwm = gpio.PWM(pin, frequency)
-            pwm.start(duty_cycle) #sets pins to PWM
+pin = [] # initialize pin variable
 
-# Define key release callback function
-def key_release(key):
-    pins = pin_mapping.get(key) #assign pins per key release
-    if pins:
-        for pin in pins:
-            pwm = gpio.PWM(pin, frequency)
-            pwm.stop() #sets pins to LO
+def press_key(event):
+    global pin # use the global pin variable
+    key = event.char.lower() # get the pressed key in lowercase
+    pin = pin_mapping.get(key, []) # get the corresponding pins for the key
+    for p in pin:
+        gpio.output(p, gpio.HIGH) # set each pin high
 
-try:
-    while True:
-        key = input("Press a WSAD key: ")
-        if not any(key in keys for keys in pin_mapping.keys()):
-            key_change_dc(key)
-            debounced_key_press = debounce(key_press)
-            debounced_key_release = debounce(key_release)
-        if key in valid_keys:
-            debounced_key_press(key)
-            time.sleep(0.1)
-            debounced_key_release(key)
-        else:
-            print("Invalid key!")
+def release_key(event):
+    global pin # use the global pin variable
+    for p in pin:
+        gpio.output(p, gpio.LOW) # set each pin low
 
-except KeyboardInterrupt:
-    gpio.cleanup()
+root = tk.Tk()
+root.bind('<KeyPress>', press_key)
+root.bind('<KeyRelease>', release_key)
+root.mainloop()
+
+gpio.cleanup()
